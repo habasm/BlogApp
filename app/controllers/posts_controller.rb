e7368c2f785b9b258a89edd72ef287aa09fe8580
Habtamu Asmare <habtasm@dtu.edu.et>
2023-05-19 10:18:49 +0300

    project revied and modified including reviewer comment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,10 @@
 class PostsController < ApplicationController
-  before_action :set_user, only: %i[index show]
-
   def index
-    @posts = @user.posts.paginate(page: params[:page], per_page: 2)
+    @user = User.find(params[:user_id])
   end
 
   def show
-    @post = @user.posts.find(params[:id])
-  end
-
-  private
-
-  def set_user
     @user = User.find(params[:user_id])
+    @post = Post.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_04_141349) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_12_115510) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.text "body", null: false
+    t.text "text"
     t.bigint "author_id", null: false
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
@@ -34,21 +34,23 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_04_141349) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "title", null: false
-    t.text "body", null: false
+    t.integer "authorid"
+    t.string "title"
+    t.text "text"
+    t.integer "comments_counter"
+    t.integer "likes_counter"
     t.bigint "author_id", null: false
-    t.integer "likes_counter", default: 0
-    t.integer "comments_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_posts_on_author_id"
+    t.index ["authorid"], name: "index_posts_on_authorid"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.text "photo"
-    t.string "bio"
-    t.integer "posts_counter", default: 0
+    t.string "photo"
+    t.text "bio"
+    t.integer "posts_count"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
